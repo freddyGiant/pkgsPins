88380de983c21a6d58ca269bf1584a959859f77c
vrad <vrad+notarealemail@vradford.com>
2026-08-15 15:09:00 -0400

    Basic flake

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "flake-parts module that deals with extra nixpkgs pins";
+
+  outputs = { self }: {
+    flakeModules.default = ./rad-flake.nix;
+    flakeModule = self.flakeModules.default;
+
+    templates.default.template = ./template;
+  };
+}
