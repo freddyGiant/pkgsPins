792509ff7920fc22d8a83d608941b775cbf6ec12
vrad <vrad+notarealemail@vradford.com>
2026-08-16 10:21:26 -0400

    templates: Reorganize
    Move default to a subdirectory of templates/ (formerly template/) to add more of them

--- flake.nix
+++ flake.nix
@@ -5,9 +5,11 @@
     flakeModules.default = ./rad-flake.nix;
     flakeModule = self.flakeModules.default;
 
-    templates.default.path = ./template;
+    templates.default.path = ./templates/default;
     templates.default.description = ''
       Barebones flake-parts boilerplate, with the rad-flake module.
     '';
+
+    # templates.shell.path = ./templates/
   };
 }
