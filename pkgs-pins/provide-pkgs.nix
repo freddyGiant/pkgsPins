656dc355613a848ff397abcb8463ee5c3c69a7ec
vrad <vrad+notarealemail@vradford.com>
2026-08-30 13:50:14 -0400

    Move business logic

---
+++ pkgs-pins/provide-pkgs.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
+let
+  makePkgs =
+    pkgsInput: system: import pkgsInput ({ inherit system; } // config.rad-flake.nixpkgsConfig);
+
+  pinNames =
+    let
+      inherit (builtins)
+        filter
+        substring
+        stringLength
+        attrNames
+        ;
+
+      prefixLength = stringLength config.polypin.inputPrefix;
+      withoutPrefix = s: substring prefixLength (stringLength s - prefixLength);
+    in
+    inputs
+    |> attrNames
+    |> filter (lib.strings.hasPrefix config.polypin.inputPrefix)
+    |> map withoutPrefix;
+
+  mkPins = system:
+  let
+    pkgsPins = lib.genAttrs' pinNames (pin: {
+      name = "pkgs-${pin}";
+      value = makePkgs inputs.${"nixpkgs-${pin}"} system;
+    });
+
+    # flake-utils handles nixpkgs -> pkgs, but if no plain nixpkgs input is present, we need to provide one of the pins
+    arbitraryPkgs = lib.optionalAttrs (!(inputs ? nixpkgs) && pinNames != [ ]) {
+      pkgs = lib.mkDefault (
+        makePkgs inputs.${"nixpkgs-${builtins.head pinNames}"} system
+      );
+    };
+  in
+    pkgsPins // arbitraryPkgs;
+in
+{
+  config.flake.polypin.pins = lib.genAttrs config.pkgs-pins.systems mkPins;
+  config.perSystem = { system, ...}: { _module.args = mkPins system; };
+}
