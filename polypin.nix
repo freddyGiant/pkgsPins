fc43ca44ec16963160dd21e74ef0dfed4b725054
vrad <vrad+notarealemail@vradford.com>
2026-08-30 13:33:55 -0400

    Clean up, add options, provide pins in a new way

--- polypin.nix
+++ polypin.nix
@@ -1,46 +1,15 @@
-# @param systems [null or [String]] If null, radFlake will generate outputs for lib.systems.flakeExposed. Otherwise, radFlake will generate outputs for these systems.
-# @param nixpkgsConfig [attrset] The arguments (aside from system) that would normally be passed to `import nixpkgs`.
-# @param useLib [null or String] If null, radFlake will arbitrarily select a lib to use from the following: inputs.nixpkgs.lib (if nixpkgs.inputs exists) and inputs.${n}.lib, where n is any "nixpkgs-"-prefixed attribute of inputs. Otherwise, radFlake will use inputs.${useLib}.lib
-# @param f [attrset -> attrset] takes system (see @param systems) parameter, pkgs/pkgs-* parameters corresponding to certain nixpkgs inputs (see @param inputs), and inputs itself and evaluates to a regular output body
-# @param inputs ; the typical inputs parameter of a flake output, but it should contain at least one set named "nixpkgs" or prefixed with "nixpkgs-" which is the output of some nixpkgs flake
-# Synopsis:
-#   radFlake args f
-#
-# Type:
-#   radFlake :: {
-#     inputs : AttrSet,
-#     systems ? null | [ String ],
-#     nixpkgsConfig ? AttrSet,
-#     useLib ? null | String
-#   } -> ({ system :: String, pkgs :: AttrSet, ... } -> AttrSet) -> AttrSet
-#
-# Inputs:
-#   args
-#     inputs
-#       `inputs` argument as passed to `outputs`. Must contain `nixpkgs` or at least one `nixpkgs-`-prefixed attribute, and all such attributes must be the outputs of nixpkgs flakes.
-#     systems
-#       Target systems. Defaults to `lib.systems.flakeExposed`.
-#     nixpkgsConfig
-#       Configuration passed to nixpkgs (e.g., `{ allowUnfree = true; }`).
-#     useLib
-#       Name of the input from which to pull `lib`. Defaults to using `inputs.nixpkgs.lib` or a `inputs.nixpkgs-*` pin, chosen arbitrarily from those provided to `inputs`.
-#
-#   f
-#     Per-system flake output function accepting `inputs`, `system`, `pkgs`, any
-#     `pkgs-<pin>` for all nixpkgs pins in `inputs`.
-# let
-#   _assert = condition: message: if !condition then throw message else null;
-# in
 {
   config,
   inputs,
   lib,
+  options,
   ...
 }:
 let
-  makePkgs = np: system: import np ({ inherit system; } // config.rad-flake.nixpkgsConfig);
+  makePkgs =
+    pkgsInput: system: import pkgsInput ({ inherit system; } // config.rad-flake.nixpkgsConfig);
 
-  altPins =
+  pinNames =
     let
       inherit (builtins)
         filter
@@ -48,33 +17,53 @@ let
         stringLength
         attrNames
         ;
+
+      prefixLength = stringLength config.polypin.inputPrefix;
+      withoutPrefix = s: substring prefixLength (stringLength s - prefixLength);
     in
     inputs
     |> attrNames
-    |> filter (n: substring 0 (stringLength "nixpkgs-") n == "nixpkgs-")
-    |> map (n: substring (stringLength "nixpkgs-") (stringLength n) n);
+    |> filter (lib.strings.hasPrefix config.polypin.inputPrefix)
+    |> map withoutPrefix;
+
+  mkPins = system:
+  let
+    pkgsPins = lib.genAttrs' pinNames (pin: {
+      name = "pkgs-${pin}";
+      value = makePkgs inputs.${"nixpkgs-${pin}"} system;
+    });
+
+    # flake-utils handles nixpkgs -> pkgs, but if no plain nixpkgs input is present, we need to provide one of the pins
+    arbitraryPkgs = lib.optionalAttrs (!(inputs ? nixpkgs) && pinNames != [ ]) {
+      pkgs = lib.mkDefault (
+        makePkgs inputs.${"nixpkgs-${builtins.head pinNames}"} system
+      );
+    };
+  in
+    pkgsPins // arbitraryPkgs;
 in
 {
-  options.rad-flake.nixpkgsConfig = lib.mkOption {
-    type = lib.types.attrs;
-    default = { };
-    description = "Extra arguments (e.g., excluding system) to pass to `import nixpkgs`.";
+  options.polypin = {
+    inputPrefix = lib.mkOption {
+      type = lib.types.str;
+      default = "nixpkgs-";
+      description = "To find and strip from input names";
+    };
+
+    outputPrefix = lib.mkOption {
+      type = lib.types.str;
+      default = "pkgs-";
+      description = "if you change this you're a weirdo";
+    };
+
+    nixpkgsConfig = lib.mkOption {
+      type = options.nixpkgs.config.type;
+      default = { };
+      description = "Extra arguments (e.g., excluding system) to pass to `import nixpkgs`.";
+    };
   };
 
-  config.perSystem =
-    { system, ... }:
-    let
-      pinArgs = lib.genAttrs' altPins (pin: {
-        name = "pkgs-${pin}";
-        value = makePkgs inputs.${"nixpkgs-${pin}"} system;
-      });
-
-      pkgsOverride = lib.optionalAttrs (!(inputs ? nixpkgs)) {
-        pkgs = lib.mkDefault (makePkgs inputs.${"nixpkgs-${builtins.head altPins}"} system);
-      };
-    in
-    assert inputs ? nixpkgs || altPins != [ ] || throw "no \"nixpkgs\" or \"nixpkgs-\"-prefixed inputs";
-    {
-      _module.args = pinArgs // pkgsOverride;
-    };
+  # this will be the user's config.systems when they import this module
+  config.flake.polypin.pins = lib.genAttrs config.systems mkPins;
+  config.perSystem = { system, ...}: { _module.args = mkPins system; };
 }
