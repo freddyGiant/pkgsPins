1d051b280299ca64e844770f0847ece49d3f8480
vrad <vrad+notarealemail@vradford.com>
2026-08-31 14:37:28 -0400

    Improve templates formats

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -1,21 +1,19 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.rad-flake.url = "github:freddyGiant/rad-flake";
+  inputs.pkgsPins.url = "github:freddyGiant/rad-flake";
 
-  outputs =
-    {
-      nixpkgs,
-      flake-parts,
-      rad-flake,
-      ...
-    }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ rad-flake.flakeModules.default ];
+  outputs = inputs:
+  inputs.flake-parts.lib.mkFlake { inherit inputs; } ({
+    inputs,
+    ...
+  }:
+  {
+    imports = [ inputs.pkgsPins.flakeModules.default ];
 
-      systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { pkgs, ... }: {
-        formatter = pkgs.nixfmt-tree;
-      };
+    systems = inputs.nixpkgs.lib.systems.flakeExposed;
+    perSystem = { pkgs, ... }: {
+      formatter = pkgs.nixfmt-tree;
     };
+  });
 }
--- templates/shell/flake.nix
+++ templates/shell/flake.nix
@@ -1,33 +1,29 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.rad-flake.url = "github:freddyGiant/rad-flake";
+  inputs.pkgsPins.url = "github:freddyGiant/rad-flake";
 
-  outputs =
-    {
-      nixpkgs,
-      flake-parts,
-      rad-flake,
-      ...
-    }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ rad-flake.flakeModules.default ];
+  outputs = inputs:
+  inputs.flake-parts.lib.mkFlake { inherit inputs; } ({
+    inputs,
+    ...
+  }:
+  {
+    imports = [ inputs.pkgsPins.flakeModules.default ];
 
-      systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        { pkgs, ... }:
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
+    systems = inputs.nixpkgs.lib.systems.flakeExposed;
+    perSystem = { pkgs, ... }: {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
 
-            ];
+        ];
 
-            shellHook = /* bash */ ''
-              #
-            '';
-          };
+        shellHook = /* bash */ ''
+          #
+        '';
+      };
 
-          formatter = pkgs.nixfmt-tree;
-        };
+      formatter = pkgs.nixfmt-tree;
     };
+  });
 }
